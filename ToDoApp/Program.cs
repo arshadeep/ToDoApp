0f820731ba3ec8439775dd70613c3151fa377ce9
Arshadeep Singh <arshu111.as@gmail.com>
2021-10-02 19:28:23 +0530

    AddedLocalStorageSupport

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ToDoApp;
 
@@ -5,5 +6,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazoredLocalStorage();//doing dependancy injection such that whenever an instance of Local storage is asked it is provided.
 await builder.Build().RunAsync();
